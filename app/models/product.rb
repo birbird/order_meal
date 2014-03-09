dca2d35f92b6eea57dcf6120d5cacddf5bd9ee20
Dan <birbird@qq.com>
2014-03-09 19:23:21 +0800

    修正计算剩余份数的方法

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ActiveRecord::Base
   has_many :quotum, :dependent => :destroy
+  has_many :orders
   
   def today_quantity
     quota = self.quotum.where(:_date => DateTime.now.to_date).first
-    return quota && quota.quantity
+    sold = self.orders.where(:_date => DateTime.now.to_date).sum("count")
+    return quota && (quota.quantity - sold)
   end
 
 end
