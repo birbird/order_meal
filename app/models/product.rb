373d9b5a93d8c82ec874ff9b18e111b867572ced
Dan <birbird@qq.com>
2014-03-09 18:32:07 +0800

    加上余量显示

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  has_many :quotum, :dependent => :destroy
+  
+  def today_quantity
+    quota = self.quotum.where(:_date => DateTime.now.to_date).first
+    return quota && quota.quantity
+  end
+
 end
