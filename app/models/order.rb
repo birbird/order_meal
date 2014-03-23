26ed40454454f7226cc4c91437047fc5def6171d
Dan <birbird@qq.com>
2014-03-23 20:16:42 +0800

    模型改为一个订单可以有多个产品

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,14 @@
 class Order < ActiveRecord::Base
-  belongs_to :product
+  has_and_belongs_to_many :products
+  
+  def product_name
+    name = ''
+    self.products.each do |product, index|
+      name = name + product.name
+      if product != self.products.last
+        name = name + '+'
+      end
+    end
+    return name
+  end
 end
---
+++ app/models/orders_products.rb
@@ -0,0 +1,4 @@
+class OrdersProducts < ActiveRecord::Base
+  belongs_to :order
+  belongs_to :product
+end
